05e0e0643e8987d1b69a9428ab9d203ef25039cf
Dipesh <kvdipesh@gmail.com>
2025-05-09 18:42:10 +0530

    Bugfix: demo images now accessible

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { NavLink } from 'react-router';
 import './App.css'
 
+import onboardingImage from './assets/images/onboarding-dark.png';
+import dashboardImage from './assets/images/dashboard-dark.png';
+
 function App() {
   return (
     <main className="app-main">
       <section className="app-section">
         <header><h1>Task 1: On-Boarding page</h1></header>
         <NavLink to="/onboarding" className="onboarding-image-container task-image-container">
-          <img className="task-image" src="./src/assets/images/onboarding-dark.png" alt="Onboarding" />
+          <img className="task-image" src={onboardingImage} alt="Onboarding" />
         </NavLink>
       </section>
       <section className="app-section">
         <header><h1>Task 2: Dashboard page</h1></header>
         <NavLink to="/dashboard" className="dashboard-image-container task-image-container">
-          <img className="task-image" src="./src/assets/images/dashboard-dark.png" alt="Dashboard" />
+          <img className="task-image" src={dashboardImage} alt="Dashboard" />
         </NavLink>
       </section>
       {/* <OnBoarding /> */}
