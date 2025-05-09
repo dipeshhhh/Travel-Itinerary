487b75cd3346bf9354bbef5a7893d3c9feae9538
Dipesh <kvdipesh@gmail.com>
2025-05-09 18:30:09 +0530

    Refactor svg to jsx and add preview images

---
+++ src/assets/icons/ArrowTopRight.jsx
@@ -0,0 +1,11 @@
+export default function ArrowTopRight({ classes }) {
+  return (
+    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        className={`svg-path svg-path-stroke ${classes}`}
+        d="M1.24241 12.5563L12.5561 1.24264M12.5561 1.24264V9.72792M12.5561 1.24264H4.07084"
+        stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
---
+++ src/assets/icons/Planner.jsx
@@ -0,0 +1,10 @@
+export default function Planner({ classes }) {
+  return (
+    <svg width="14" height="16" viewBox="0 0 14 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        className={`svg-path svg-path-fill ${classes}`}
+        d="M10.75 2V6.5L9.25 5L7.75 6.5V2H4.75V14H12.25V2H10.75ZM0.25 4.25V2.75H1.75V2C1.75 1.60218 1.90804 1.22064 2.18934 0.93934C2.47064 0.658035 2.85218 0.5 3.25 0.5H12.25C13.0375 0.5 13.75 1.2125 13.75 2V14C13.75 14.7875 13.0375 15.5 12.25 15.5H3.25C2.4625 15.5 1.75 14.7875 1.75 14V13.25H0.25V11.75H1.75V8.75H0.25V7.25H1.75V4.25H0.25ZM1.75 2.75V4.25H3.25V2.75H1.75ZM1.75 13.25H3.25V11.75H1.75V13.25ZM1.75 8.75H3.25V7.25H1.75V8.75Z"
+        fill="#D1F462" />
+    </svg>
+  )
+}
